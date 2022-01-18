57740fcea9416c0003cc22691fc47bed73a9d09d
Felipe Martins <mfelipeluiz99@gmail.com>
2022-01-18 13:03:49 -0300

    Req 2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const NotFound = 'Pessoa palestrante não encontrada';
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
@@ -16,6 +17,15 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (req, res) => {
   const talker = await fs.readFile('talker.json', 'utf-8');
   return res.status(200).json(JSON.parse(talker));
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await fs.readFile('talker.json', 'utf-8');
+  const talkerId = JSON.parse(talker).find((r) => r.id === +id);
+
+  if (!talkerId) return res.status(404).json({ message: `${NotFound}` });
+  return res.status(200).json(talkerId);
 });
 
 app.listen(PORT, () => {
